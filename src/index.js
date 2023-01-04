d8f75abcfb4bad8eb36212a9ce62dee9e45e3944
jayam04 <jayam04@proton.me>
2023-01-04 16:33:49 +0530

    some buttons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './js/Datagrid';
+import MyButton  from './js/Button';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <MyButton />
   </React.StrictMode>
 );
 
