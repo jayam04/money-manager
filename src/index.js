c96891720d95cbf50b2482d90630c0368cbac08f
jayam04 <jayam04@proton.me>
2023-01-05 18:46:52 +0530

    fixed error with react build

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './js/Datagrid';
-import MyButton  from './js/Button';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <MyButton />
   </React.StrictMode>
 );
 
