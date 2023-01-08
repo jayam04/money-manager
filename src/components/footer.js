3979eb1ccb00a4243b578a44996fdd0c0efab397
Jayam Patel <jayam04@proton.me>
2023-01-08 11:15:55 +0530

    footer for website (#2)
    * trash commit
    
    * removed unused ReactDOM
    
    * changed name of private folder
    
    * added testing buttons
    
    * removed unused imports
    
    * redesigned footbar and stacked all functions in one file
    
    * final footer for now!

---
+++ src/components/footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { UilGithubAlt } from "@iconscout/react-unicons";
+import "./footer.sass";
+
+function Footer() {
+  return (
+    <div className="main">
+      {/* reachus */}
+      <div className="reachus">
+        <a href="https://github.com/jayam04/monymngr">
+          <UilGithubAlt size="20" />
+        </a>
+      </div>
+
+      {/* attributes */}
+      <div className="attributes">
+        <h4>attributes</h4>
+        <div className="attributes">
+          <p>
+            <a
+              href="https://www.flaticon.com/free-icon/money-management_9165762?term=money+manager&page=1&position=63&origin=search&related_id=9165762"
+              title="logo"
+            >
+              logo
+            </a>{" "}
+            by <a href="https://www.flaticon.com/authors/nawicon">nawicon </a>in{" "}
+            <a href="https://www.flaticon.com/">flaticon</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Footer from "./footer/Footer.js"
+import Footer from "./components/footer.js";
 
 // from old file
-import './index.css';
+import './index.sass';
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <button onClick={alert('website under maintenence!!')}>hello</button>
+    
+    <button className='mdc-button'>
+    <span class="mdc-button__ripple"></span>
+    <span class="mdc-button__touch"></span>
+    <span className='mdc-button__focus-ring'></span>
+      hello
+    </button>
     <Footer />
   </React.StrictMode>
 );
