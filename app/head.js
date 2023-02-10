5b1c999c5bd0a50198f60800357d9f607ace07d6
Jayam Patel <93824505+jayam04@users.noreply.github.com>
2023-02-10 19:16:40 +0530

    changed google search verification method

--- app/head.js
+++ app/head.js
@@ -3,6 +3,9 @@ export default function RootHead() {
         <head>
             <title>monymngr</title>
             <meta charSet='utf-8' />
+
+            {/* google search console verification */}
+            <meta name="google-site-verification" content="gbQDdQ1IawODcdAJcR9WfQjXTHBo5MsB5S3denFl1A4" />
         </head>
     )
 }
--- next.config.js
+++ next.config.js
@@ -7,10 +7,5 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-rewrites: async () => [
-  {
-    source: "/public/myfile.html",
-    destination: "/pages/api/myfile.js"
-  }
-]
+
 
