5512a1fdd7756b57a6bee8932bba9b75fc475811
Jayam Patel <93824505+jayam04@users.noreply.github.com>
2023-02-11 11:01:06 +0530

    testing 'use client' in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { AnalyticsWrapper } from '../components/analytics';
 import Navmenu from "../components/navmenu"
 import Warning from "../components/development"
@@ -12,7 +14,7 @@ export default function RootLayout({ children }: {
     children: React.ReactNode;
   }) {
     return (
-      <html lang="en-US">
+      <html>
         <head />
         <body>
             {devlopmentStatus
@@ -32,6 +34,26 @@ export default function RootLayout({ children }: {
             <AnalyticsWrapper />
         </body>
       </html>
+      // <html lang="en-US">
+      //   <head />
+      //   <body>
+      //       {devlopmentStatus
+      //           ? <Warning />
+      //           : <></>
+      //       }
+      //       <div className="root">
+      //           <div className="root_sidebar">
+      //               <Navmenu />
+      //           </div>
+      //           <div className="root_children">
+      //               {children}
+      //           </div>
+      //       </div>
+
+
+      //       <AnalyticsWrapper />
+      //   </body>
+      // </html>
     );
   }
   
