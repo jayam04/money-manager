44ebfefe824cefe1f5f3f9ad2d9df4fc830a1704
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-06-17 12:54:51 +0530

    added shadcnui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,13 @@
 import Navbar from "@/components/Navbar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Dashboard() {
   return (
-    <div className="grid">
+    <div className="flex flex-col h-screen justify-between">
+        <Avatar>
+          <AvatarImage src="jayampatel.png" />
+          <AvatarFallback>JP</AvatarFallback>
+        </Avatar>
         <Navbar />
     </div>
   )
