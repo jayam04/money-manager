38b9b268784067adcf4a12da0b3cb890ba8c1938
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-06-21 22:40:34 +0530

    added table component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import { AlertCircle, FileWarning, Terminal } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+let developmentBuild = true;
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,6 +22,17 @@ export default function RootLayout({
   return (
     <html lang="en-US">
       <body className={inter.className}>
+        {developmentBuild ? (
+          <Alert variant="destructive" className="w-7/8 m-1">
+            <AlertCircle className="h-4 w-4" />
+            <AlertTitle>NOTICE!</AlertTitle>
+            <AlertDescription>
+              You are using a development build of the application.
+            </AlertDescription>
+          </Alert>
+        ) : (
+          <></>
+        )}
         {/* main body */}
         <div className="h-auto" style={{ height: "calc(100vh - 60px)" }}>
           {children}
