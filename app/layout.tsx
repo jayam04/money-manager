16e434da1ebaf25d831a02a32d0220fb238c7209
Jayam Patel <93824505+jayam04@users.noreply.github.com>
2023-06-23 08:20:59 +0530

    Implementation of shadcnui (#13)
    * added shadcnui
    
    * some ui components
    
    * updated navbar looks
    
    * added table component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,50 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+import { AlertCircle, FileWarning, Terminal } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+let developmentBuild = true;
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'monymngr',
-  description: 'your finance manager',
-}
+  title: "monymngr",
+  description: "your finance manager",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en-US">
       <body className={inter.className}>
-        {children}
-        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"></link>
+        {developmentBuild ? (
+          <Alert variant="destructive" className="w-7/8 m-1">
+            <AlertCircle className="h-4 w-4" />
+            <AlertTitle>NOTICE!</AlertTitle>
+            <AlertDescription>
+              You are using a development build of the application.
+            </AlertDescription>
+          </Alert>
+        ) : (
+          <></>
+        )}
+        {/* main body */}
+        <div className="h-auto" style={{ height: "calc(100vh - 60px)" }}>
+          {children}
+        </div>
+        {/* navbar  */}
+        <Navbar />
+        {/* unicons css */}
+        <link
+          rel="stylesheet"
+          href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"
+        ></link>
       </body>
     </html>
-  )
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
 import NavbarItem from "./NavbarItem";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-row justify-between h-14 bg-red-200 rounded-2xl mx-2 mb-1">
         <NavbarItem to="/" name="Home" uniconIconName="chart-pie" />
+        <NavbarItem to="/dashboard" name="Dashboard" uniconIconName="chart-bar" />
         <NavbarItem to="/attributes" name="Attributes" uniconIconName="setting" />
     </div>
   )
