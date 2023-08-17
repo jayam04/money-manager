e4e1c93797a5891d9789cf37c6149f12dd82d0d0
jayam04 <jayampatel04@gmail.com>
2023-08-17 19:44:26 +0530

    added wallets and categories section

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 
 export function Sidebar() {
-    const hover_text = "hover:text-blue-500 hover:text-underline"
+    const hover_text = "hover:text-blue-500 hover:text-underline text-lg"
     return(
         <div className="w-full flex flex-col">
             <h1 className="font-mono text-3xl font-bold">monymngr</h1>
             <hr className="mr-16"/>
             <Link className={hover_text} href="/">dashboard</Link>
             <Link className={hover_text} href="/transactions">transactions</Link>
+            <Link className={hover_text} href="/wallets">wallets</Link>
+            <Link className={hover_text} href="/categories">categories</Link>
             <Link className={hover_text} href="/settings">settings</Link>
             <Link className={hover_text} href="/attributes">about</Link>
         </div>
