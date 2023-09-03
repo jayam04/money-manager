9a8fcd5a3769b81ecdc44a68e626fec252fe6061
Jayam Patel <jayam04@proton.me>
2023-09-03 23:57:03 +0530

    Radixui (#16)
    * removed all files
    
    * basic nextjs app
    
    * some basic pages
    
    * removed unused import
    
    * added shadcnui
    
    * added shadcnui (#12)
    
    * some ui components
    
    * updated navbar looks
    
    * added table component
    
    * Implementation of shadcnui (#13)
    
    * added shadcnui
    
    * some ui components
    
    * updated navbar looks
    
    * added table component
    
    * table
    
    * new transactions page
    
    * fixed invalid imports
    
    * added more fields in transactions
    
    * fixed typo in placeholder
    
    * channged different placeholder in date picker
    
    * fixed eslint issue
    
    * some minor changes
    
    * some temp transactions
    
    * make some tweaks
    
    * added wallets and categories section
    
    * using pnpm package manager
    
    * using pnpm package managert
    
    * updated multi-wallet transactions
    
    * added transactions
    
    * fixed linting issue
    
    * updated transactions layout

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -14,49 +14,9 @@ import {
 
 import { format } from 'date-fns'; // Import the format function from date-fns
 
+import { transactions, accounts } from "@/data/transactions";
+
 export default function TransactionsPage() {
-    
-    var transactions = [
-        {
-          id: 0,
-          description: "burger?",
-          wallet: "cash?",
-          date: new Date("2023-07-30"),
-          amount: 3.67,
-        },
-      ];
-      
-      // Function to generate a random description for the items
-      function generateRandomDescription() {
-        const descriptions = ["coffee", "groceries", "gas", "movie tickets", "dinner", "clothes", "books", "electronics"];
-        const randomIndex = Math.floor(Math.random() * descriptions.length);
-        return descriptions[randomIndex];
-      }
-      
-      // Function to generate a random wallet type for the items
-      function generateRandomWallet() {
-        const wallets = ["cash", "credit card", "debit card", "mobile payment"];
-        const randomIndex = Math.floor(Math.random() * wallets.length);
-        return wallets[randomIndex];
-      }
-      
-      // Function to generate a random amount for the items
-      function generateRandomAmount() {
-        return Math.random() * 100;
-      }
-      
-      // Add 100 more items
-      for (let i = 1; i <= 100; i++) {
-        transactions.push({
-          id: i,
-          description: generateRandomDescription(),
-          wallet: generateRandomWallet(),
-          date: new Date(), // Use the current date and time for new items
-          amount: generateRandomAmount(),
-        });
-      }
-      
-      console.log(transactions); // This will display the updated `transactions` dictionary with 101 items
       
   return (
     <>
@@ -66,21 +26,33 @@ export default function TransactionsPage() {
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">id</TableHead>
+            {/* <TableHead className="w-[100px]">id</TableHead> */}
             <TableHead>description</TableHead>
-            <TableHead>wallet</TableHead>
+            {/* <TableHead>wallet</TableHead> */}
             <TableHead>date</TableHead>
+            <TableHead>category</TableHead>
             <TableHead className="text-right font-mono">amount</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
             {transactions.map((transaction) => (
                 <TableRow key={transaction.id} className="justify-right">
-                    <TableCell>{transaction.id}</TableCell>
+                    {/* <TableCell>{transaction.id}</TableCell> */}
                     <TableCell>{transaction.description}</TableCell>
-                    <TableCell>{transaction.wallet}</TableCell>
-                    <TableCell>{format(transaction.date, 'MM/dd/yyyy')}</TableCell>
-                    <TableCell className="font-mono text-right">{transaction.amount}</TableCell>
+                    {/* <TableCell>{transaction.wallet}</TableCell> */}
+                    {/* <TableCell>{format(transaction.date, 'MM/dd/yyyy')}</TableCell> */}
+                    <TableCell>{transaction.date}</TableCell>
+                    <TableCell>{transaction.category} / {transaction.subcategory}</TableCell>
+                    {/* <TableCell className="font-mono text-right">{transaction.amount}</TableCell> */}
+                    <TableCell>
+                        {Object.entries(transaction.accounts).map(([key, value]) => 
+                            <div key={key} className="flex flex-row">
+                                <p className="font-mono w-20">{value} {accounts[key]["currency"]}</p>
+                                <p>{accounts[key]["name"]}</p>
+                            </div>
+                        )}
+                    </TableCell>
+
                 </TableRow>
             ))}
         </TableBody>
---
+++ data/transactions.ts
@@ -0,0 +1,200 @@
+export const transactions = [
+    {
+      "id": "transaction12",
+      "date": "2020-01-01",
+      "description": "Burger",
+      "type": "expense",
+      "category": "Food",
+      "subcategory": "Fast Food",
+      "accounts": {
+        "3": -99
+      }
+    },
+    {
+      "id": "transaction10",
+      "date": "2020-01-11",
+      "description": "Shoes",
+      "type": "expense",
+      "category": "Shopping",
+      "subcategory": "Shoes",
+      "accounts": {
+        "0": -100,
+        "4": -345
+      }
+    },
+    {
+      "id": "transaction11",
+      "date": "2020-01-12",
+      "description": "Cashback on Shoes",
+      "type": "income",
+      "category": "Offers and Cashback",
+      "subcategory": "Cashback",
+      "accounts": {
+        "4": 34
+      }
+    },
+    {
+      "id": "transaction13",
+      "date": "2020-01-13",
+      "description": "Gas",
+      "type": "expense",
+      "category": "Transportation",
+      "subcategory": "Gas",
+      "accounts": {
+        "2": -500
+      }
+    },
+    {
+      "id": "transaction14",
+      "date": "2020-01-13",
+      "description": "Transfer",
+      "type": "transfer",
+      "category": "Transfer",
+      "subcategory": "Accounts",
+      "accounts": {
+        "2": -7298,
+        "0": 89,
+      }
+    },
+    {
+      "id": "transaction1",
+      "date": "2020-01-02",
+      "description": "Expense 1",
+      "type": "expense",
+      "category": "Food",
+      "subcategory": "Groceries",
+      "accounts": {
+        "0": -20,
+        "1": 20
+      }
+    },
+    {
+      "id": "transaction2",
+      "date": "2020-01-03",
+      "description": "Expense 2",
+      "type": "expense",
+      "category": "Entertainment",
+      "subcategory": "Movies",
+      "accounts": {
+        "0": -15,
+        "2": 15
+      }
+    },
+    {
+      "id": "transaction3",
+      "date": "2020-01-04",
+      "description": "Expense 3",
+      "type": "expense",
+      "category": "Transportation",
+      "subcategory": "Gas",
+      "accounts": {
+        "0": -30,
+        "3": 30
+      }
+    },
+    {
+      "id": "transaction4",
+      "date": "2020-01-05",
+      "description": "Expense 4",
+      "type": "expense",
+      "category": "Shopping",
+      "subcategory": "Clothing",
+      "accounts": {
+        "0": -50,
+        "1": 50
+      }
+    },
+    {
+      "id": "transaction5",
+      "date": "2020-01-06",
+      "description": "Expense 5",
+      "type": "expense",
+      "category": "Food",
+      "subcategory": "Dining Out",
+      "accounts": {
+        "0": -40,
+        "2": 40
+      }
+    },
+    {
+      "id": "transaction6",
+      "date": "2020-01-07",
+      "description": "Expense 6",
+      "type": "expense",
+      "category": "Entertainment",
+      "subcategory": "Concert",
+      "accounts": {
+        "0": -60,
+        "3": 60
+      }
+    },
+    {
+      "id": "transaction7",
+      "date": "2020-01-08",
+      "description": "Expense 7",
+      "type": "expense",
+      "category": "Shopping",
+      "subcategory": "Electronics",
+      "accounts": {
+        "0": -70,
+        "1": 70
+      }
+    },
+    {
+      "id": "transaction8",
+      "date": "2020-01-09",
+      "description": "Expense 8",
+      "type": "expense",
+      "category": "Food",
+      "subcategory": "Groceries",
+      "accounts": {
+        "0": -25,
+        "2": 25
+      }
+    },
+    {
+      "id": "transaction9",
+      "date": "2020-01-10",
+      "description": "Expense 9",
+      "type": "expense",
+      "category": "Transportation",
+      "subcategory": "Bus Fare",
+      "accounts": {
+        "0": -10,
+        "3": 10
+      }
+    }
+];
+
+export const accounts: {[key: string]: {name: string, type: string, currency: string, color: string}} = {
+    "0": {
+        "name": "Cash",
+        "type": "Wallet",
+        "currency": "USD",
+        "color": "#ff0000"
+    },
+    "1": {
+        "name": "Bank 1",
+        "type": "Bank",
+        "currency": "INR",
+        "color": "#ff0000"
+    },
+    "2": {
+        "name": "Cash (INR)",
+        "type": "Wallet",
+        "currency": "INR",
+        "color": "#ff0000"
+    },
+    "3": {
+        "name": "Big Savings Credit Card",
+        "type": "Credit Card",
+        "currency": "INR",
+        "color": "#ff0000"
+    },
+    "4": {
+        "name": "Amazon Wallet",
+        "type": "Wallet",
+        "currency": "USD",
+        "color": "#ff0000"
+    }
+}
