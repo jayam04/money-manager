aa1c33c5d857fa4444e116c9d82bcd2a801c6412
jayam04 <jayampatel04@gmail.com>
2023-09-04 23:12:27 +0530

    checking firebase auth

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,10 +1,26 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Table, TableCaption, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+// import { firebaseConfig } from "@/js/firebase/firebaseConfig";
 
 
 import { transactions } from "@/data/transactions";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+
+  const app = initializeApp({
+    apiKey: "AIzaSyCNDZnKFgve9y9WzbsGJtodDqTwH3OBv8Y",
+    authDomain: "monymngr.firebaseapp.com",
+    databaseURL: "https://monymngr-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "monymngr",
+    storageBucket: "monymngr.appspot.com",
+    messagingSenderId: "223749319252",
+    appId: "1:223749319252:web:582ebcc56c18f28b671bc1",
+    measurementId: "G-P51ZM058YE"
+});
+  const provider = new GoogleAuthProvider();
+  await signInWithRedirect(getAuth(app), provider);
   return (
     <div className="flex flex-col justify-between">
       <Avatar>
