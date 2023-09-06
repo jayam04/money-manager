3583933b3ae8cd9bbf7e5e1b01b786a7305dceb6
jayam04 <jayampatel04@gmail.com>
2023-09-06 19:35:43 +0530

    added auth in settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,11 @@
+import { SignInSignOutBundle, SignInWithGoogleRedirect, SignOutButton } from "@/components/AuthComponents";
 import { PageHeading } from "@/components/Headings";
 
 export default function SettingsPage() {
   return(
     <>
     <PageHeading heading={"settings"} />
+    <SignInSignOutBundle />
     </>
   )
 }
