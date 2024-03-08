faaf88ca0518310b4322b8f1c83ee64367b3b90d
jayam04 <jayampatel04@gmail.com>
2024-03-08 16:11:36 +0530

    added messy code, again!

--- app/construction/page.tsx
+++ app/construction/page.tsx
@@ -6,7 +6,7 @@ export default function ConstructionPage() {
     return (
         <div>
             <h1>Construction Page</h1>
-            <p>Here are some helpful link that can help work done, until website isn't fully built</p>
+            <p>{`Here are some helpful link that can help work done, until website isn't fully built`}</p>
             <Link href="helpers/1">Helpers/1</Link>
         </div>
     )
