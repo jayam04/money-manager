fac59850dfac14974fe8aa1bd191ca85b1d10d2b
jayam04 <jayampatel04@gmail.com>
2024-03-10 22:21:08 +0530

    fixed pnpm lock file

--- lib/firebase/constants.ts
+++ lib/firebase/constants.ts
@@ -5,5 +5,4 @@ export const firebaseConfig = {
   storageBucket: "spends-crimsontwilight.appspot.com",
   messagingSenderId: "62092747251",
   appId: "1:62092747251:web:a51e286794ef79a18c7286"
-r
 };
